1a34339699dcf29e1362d4c1a13c3d8b96dafd9b
William Best <wmbest2@gmail.com>
2014-04-15 15:19:09 -0500

    Updated unmarshaller to work with all xml files

--- apk/xml.go
+++ apk/xml.go
@@ -1,95 +1,160 @@
 package apk
 
 import (
-	"encoding/xml"
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
 	"math"
+    "bytes"
+    "encoding/xml"
 )
+
+const (
+	CHUNK_AXML_FILE           = 0x00080003
+	CHUNK_RESOURCEIDS         = 0x00080180
+	CHUNK_STRINGS             = 0x001C0001
+	CHUNK_XML_END_NAMESPACE   = 0x00100101
+	CHUNK_XML_END_TAG         = 0x00100103
+	CHUNK_XML_START_NAMESPACE = 0x00100100
+	CHUNK_XML_START_TAG       = 0x00100102
+	CHUNK_XML_TEXT            = 0x00100104
+	UTF8_FLAG                 = 0x00000100
+	SKIP_BLOCK                = 0xFFFFFFFF
+)
+
+type stringsMeta struct {
+	Nstrings         uint32
+	StyleOffsetCount uint32
+	Flags            uint32
+	StringDataOffset uint32
+	Stylesoffset     uint32
+    DataOffset		[]uint32
+}
 
 // decompressXML -- Parse the 'compressed' binary form of Android XML docs
 // such as for AndroidManifest.xml in .apk files
-func Unmarshal(body []byte, manifest *Manifest) error {
-	endDocTag := 0x00100101
-	startTag := 0x00100102
-	endTag := 0x00100103
+func Unmarshal(b []byte, manifest interface{}) error {
 
-	// Compressed XML file/bytes starts with 24x bytes of data,
-	// 9 32 bit words in little endian order (LSB first):
-	//   0th word is 03 00 08 00
-	//   3rd word SEEMS TO BE:  Offset at then of StringTable
-	//   4th word is: Number of strings in string table
-	// WARNING: Sometime I indiscriminently display or refer to word in
-	//   little endian storage format, or in integer format (ie MSB first).
-	numbStrings := lew(body, 4*4)
+    body := bytes.NewReader(b)
 
-	// StringIndexTable starts at offset 24x, an array of 32 bit LE offsets
-	// of the length/string data in the StringTable.
-	sitOff := 0x24 // Offset of start of StringIndexTable
+	var blocktype, size, indent, header uint32
+    var stringsData stringsMeta
 
-	// StringTable, each string is represented with a 16 bit little endian
-	// character count, followed by that number of 16 bit (LE) (Unicode) chars.
-	stOff := sitOff + numbStrings*4 // StringTable follows StrIndexTable
+    // Check Header
+	binary.Read(body, binary.LittleEndian, &header)
+	if header != CHUNK_AXML_FILE {
+		return errors.New("AXML file has wrong header")
+	}
 
-	// XMLTags, The XML tag tree starts after some unknown content after the
-	// StringTable.  There is some unknown data after the StringTable, scan
-	// forward from this point to the flag for the start of an XML start tag.
-	bodyTagOff := lew(body, 3*4) // Start from the offset in the 3rd word.
-	// Scan forward until we find the bytes: 0x02011000(x00100102 in normal int)
-	for ii := bodyTagOff; ii < len(body)-4; ii += 4 {
-		if lew(body, ii) == startTag {
-			bodyTagOff = ii
-			break
-		}
-	} // end of hack, scanning for start of first start tag
+    // Check filesize
+	binary.Read(body, binary.LittleEndian, &header)
+	if int(header) != len(b) {
+		return errors.New("AXML file has the wrong size")
+	}
 
-	// XML tags and attributes:
-	// Every XML start and end tag consists of 6 32 bit words:
-	//   0th word: 02011000 for startTag and 03011000 for endTag
-	//   1st word: a flag?, like 38000000
-	//   2nd word: Line of where this tag appeared in the original source file
-	//   3rd word: FFFFFFFF ??
-	//   4th word: StringIndex of NameSpace name, or FFFFFFFF for default NS
-	//   5th word: StringIndex of Element Name
-	//   (Note: 01011000 in 0th word means end of XML document, endDocTag)
+    var output string
+	// Start offset at 8 bytes for header and size
+	for offset := uint32(8); offset < header; {
+		var lineNumber, skip, nsIdx, nameIdx, flag uint32
+		binary.Read(body, binary.LittleEndian, &blocktype)
+		binary.Read(body, binary.LittleEndian, &size)
+        if  blocktype != CHUNK_RESOURCEIDS && blocktype != CHUNK_STRINGS {
+			binary.Read(body, binary.LittleEndian, &lineNumber)
+			binary.Read(body, binary.LittleEndian, &skip)
+			if skip != SKIP_BLOCK {
+				return errors.New("Error: Expected block 0xFFFFFFFF")
+			}
+			binary.Read(body, binary.LittleEndian, &nsIdx)
+			binary.Read(body, binary.LittleEndian, &nameIdx)
+			binary.Read(body, binary.LittleEndian, &flag)
+        }
+		switch blocktype {
+		default:
+			return fmt.Errorf("Unkown chunk type: %X", blocktype)
+		case CHUNK_RESOURCEIDS:
+		case CHUNK_STRINGS:
+			/* +------------------------------------+
+			 * | Nstrings         uint32            |
+			 * | StyleOffsetCount uint32            |
+			 * | Flags            uint32            |
+			 * | StringDataOffset uint32            |
+			 * | flag             uint32            |
+			 * | Stylesoffset     uint32            |
+			 * +------------------------------------+
+			 * | +--------------------------------+ |
+			 * | | DataOffset uint32              | |
+			 * | +--------------------------------+ |
+			 * |       Repeat Nstrings times        |
+			 * +------------------------------------+
+			 * |
+			 * +------------------------------------+
+			 */
+			binary.Read(body, binary.LittleEndian, &stringsData.Nstrings)
+			binary.Read(body, binary.LittleEndian, &stringsData.StyleOffsetCount)
+			binary.Read(body, binary.LittleEndian, &stringsData.Flags)
+			binary.Read(body, binary.LittleEndian, &stringsData.StringDataOffset)
+			binary.Read(body, binary.LittleEndian, &stringsData.Stylesoffset)
 
-	// Start tags (not end tags) contain 3 more words:
-	//   6th word: 14001400 meaning??
-	//   7th word: Number of Attributes that follow this tag(follow word 8th)
-	//   8th word: 00000000 meaning??
+            for i := uint32(0); i < stringsData.Nstrings; i++ {
+				var offset uint32
+				binary.Read(body, binary.LittleEndian, &offset)
+                stringsData.DataOffset = append(stringsData.DataOffset, offset)
+			}manifest
+            stringsData.StringDataOffset = 0x24 + stringsData.Nstrings * 4
+		case CHUNK_XML_END_NAMESPACE:
+		case CHUNK_XML_END_TAG:
+			indent--
+			name := compXmlStringAt(body, stringsData, nameIdx)
+			output = fmt.Sprintf("%s%s</%s>\n", output, computeIndent(indent), name)
+		case CHUNK_XML_START_NAMESPACE:
+		case CHUNK_XML_START_TAG:
+			/* +----------------------------- w-------+
+			 * | lineNumber     uint32              |
+			 * | skip           uint32 = SKIP_BLOCK |
+			 * | nsIdx          uint32              |
+			 * | nameIdx        uint32              |
+			 * | flag           uint32 = 0x00140014 |
+			 * | attributeCount uint16              |
+			 * +------------------------------------+
+			 * | +--------------------------------+ |
+			 * | | nsIdx       uint32             | |
+			 * | | nameIdx     uint32             | |
+			 * | | valueString uint32 // Skipped  | |
+			 * | | aValueType  uint32             | |
+			 * | | aValue      uint32             | |
+			 * | +--------------------------------+ |
+			 * |   Repeat attributeCount times      |
+			 * +------------------------------------+
+			 */
 
-	// Attributes consist of 5 words:
-	//   0th word: StringIndex of Attribute Name's Namespace, or FFFFFFFF
-	//   1st word: StringIndex of Attribute Name
-	//   2nd word: StringIndex of Attribute Value, or FFFFFFF if ResourceId used
-	//   3rd word: Flags?
-	//   4th word: str ind of attr value again, or ResourceId of value
+			var attributeCount, junk uint32
+			// Check if flag is magick number
+			// https://code.google.com/p/axml/source/browse/src/main/java/pxb/android/axml/AxmlReader.java?r=9bc9e64ef832736a93750998a9fa1d4406b858c3#102
+			if flag != 0x00140014 {
+				return fmt.Errorf("Expected flag 0x00140014, found %08X at %08X\n", flag, offset+4*6)
+			}
 
-	// Step through the XML tree element tags and attributes
-	var output string
-	off := bodyTagOff
-	indent := 0
-	for off < len(body) {
-		tag0 := lew(body, off)
-		nameSi := lew(body, off+5*4)
+            name := compXmlStringAt(body, stringsData, nameIdx)
 
-		if tag0 == startTag { // XML START TAG
-			numbAttrs := lew(body, off+7*4) // Number of Attributes to follow
-			off += 9 * 4                    // Skip over 6+3 words of startTag data
-			name := compXmlString(body, sitOff, stOff, nameSi)
+			binary.Read(body, binary.LittleEndian, &attributeCount)
+            binary.Read(body, binary.LittleEndian, &junk)
 
 			var att string
 			// Look for the Attributes
-			for ii := 0; ii < numbAttrs; ii++ {
-				attrNameSi := lew(body, off+1*4)  // AttrName String Index
-				attrValueSi := lew(body, off+2*4) // AttrValue Str Ind, or FFFFFFFF
-				attrResId := lew(body, off+4*4)   // AttrValue ResourceId or dup AttrValue StrInd
-				off += 5 * 4                      // Skip over the 5 words of an attribute
+            for i := 0; i < int(attributeCount); i++ {
+                var attrNameSi, attrNSSi, attrValueSi, flags, attrResId uint32 
+                binary.Read(body, binary.LittleEndian, &attrNSSi)
+                binary.Read(body, binary.LittleEndian, &attrNameSi)
+                binary.Read(body, binary.LittleEndian, &attrValueSi)
+                binary.Read(body, binary.LittleEndian, &flags)
+                binary.Read(body, binary.LittleEndian, &attrResId)
 
-				attrName := compXmlString(body, sitOff, stOff, attrNameSi)
+				attrName := compXmlStringAt(body, stringsData, attrNameSi)
 
 				var attrValue string
 				if attrValueSi != 0xffffffff {
-					attrValue = compXmlString(body, sitOff, stOff, attrValueSi)
+					attrValue = compXmlStringAt(body, stringsData, attrValueSi)
 				} else {
 					attrValue = fmt.Sprintf("%d", attrResId)
 				}
@@ -98,32 +163,15 @@ func Unmarshal(body []byte, manifest *Manifest) error {
 
 			output = fmt.Sprintf("%s%s<%s%s>\n", output, computeIndent(indent), name, att)
 			indent++
-		} else if tag0 == endTag { // XML END TAG
-			indent--
-			off += 6 * 4 // Skip over 6 words of endTag data
-			name := compXmlString(body, sitOff, stOff, nameSi)
-			output = fmt.Sprintf("%s%s</%s>\n", output, computeIndent(indent), name)
-
-		} else if tag0 == endDocTag { // END OF XML DOC TAG
-			break
-		} else {
-			output = fmt.Sprintf("%s  Unrecognized tag code '%d' at offset %d\n", output, tag0, off)
-			break
+		case CHUNK_XML_TEXT:
 		}
-	} // end of while loop scanning tags and attributes of XML tree
-	//fmt.Printf("%s    end at offset %d\n", output, off)
-	return xml.Unmarshal([]byte(output), manifest)
-} // end of decompressXML
-
-func compXmlString(xml []byte, sitOff int, stOff int, strInd int) string {
-	if strInd < 0 {
-		return ""
+		offset += size
+		body.Seek(int64(offset), 0)
 	}
-	strOff := stOff + lew(xml, sitOff+strInd*4)
-	return compXmlStringAt(xml, strOff)
+    return xml.Unmarshal([]byte(output), manifest)
 }
 
-func computeIndent(indent int) string {
+func computeIndent(indent uint32) string {
 	spaces := string("                                             ")
 	m := int(math.Min(float64(indent*2), float64(len(spaces))))
 	return spaces[:m]
@@ -132,18 +180,28 @@ func computeIndent(indent int) string {
 // compXmlStringAt -- Return the string stored in StringTable format at
 // offset strOff.  This offset points to the 16 bit string length, which
 // is followed by that number of 16 bit (Unicode) chars.
-func compXmlStringAt(arr []byte, strOff int) string {
-	strLen := int(arr[strOff+1])<<8&0xff00 | int(arr[strOff])&0xff
+func compXmlStringAt(arr io.ReaderAt, meta stringsMeta, strOff uint32) string {
+    if strOff == 0xffffffff {
+        return ""
+    }
+    length := make([]byte, 2)
+    off := meta.StringDataOffset + meta.DataOffset[strOff]
+	arr.ReadAt(length, int64(off))
+	strLen := int(length[1] << 8 + length[0])
 
-	chars := make([]byte, strLen)
-	for i, _ := range chars {
-		chars[i] = arr[strOff+2+i*2]
+	chars := make([]byte, int64(strLen))
+    ii := 0
+    for i := 0; i < strLen; i++ {
+        c := make([]byte, 1)
+        arr.ReadAt(c, int64(int(off) + 2 + ii))
+
+        if c[0] == 0 {
+            i--
+        } else {
+            chars[i] = c[0]
+        }
+        ii++
 	}
-	return string(chars) // Hack, just use 8 byte chars
-} // end of compXmlStringAt
 
-// lew -- Return value of a Little Endian 32 bit word from the byte array
-//   at offset off.
-func lew(arr []byte, off int) int {
-	return int(arr[off+3])<<24&0xff000000 | int(arr[off+2])<<16&0xff0000 | int(arr[off+1])<<8&0xff00 | int(arr[off])&0xFF
-} // end of lew
+	return string(chars) 
+} // end of compXmlStringAt
