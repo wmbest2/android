52ce8f86a000d0cb723e7f27e9de5b78d6bb5258
William Best <wmbest2@gmail.com>
2014-04-15 08:47:59 -0500

    Added more to Manifest model

--- apk/apk.go
+++ apk/apk.go
@@ -1,6 +1,53 @@
 package apk
 
+/*
+
+<instrumentation android:functionalTest=["true" | "false"]
+     android:handleProfiling=["true" | "false"]
+     android:icon="drawable resource"
+     android:label="string resource"
+     android:name="string"
+     android:targetPackage="string" />
+*/
+type Instrumentation struct {
+    Name string `xml:"name,attr"`
+    Target string `xml:"targetPackage,attr"`
+    HandleProfiling bool `xml:"handleProfiling,attr"`
+    FunctionalTest bool `xml:"functionalTest,attr"`
+}
+
+type Application struct {
+    AllowTaskReparenting bool `xml:"allowTaskReparenting,attr"`
+    AllowBackup bool `xml:"allowBackup,attr"`
+    BackupAgent string `xml:"backupAgent,attr"`
+    Debuggable bool `xml:"debuggable,attr"`
+    Description string `xml:"description,attr"`
+    Enabled bool `xml:"enabled,attr"`
+    HasCode bool `xml:"hasCode,attr"`
+    HardwareAccelerated bool `xml:"hardwareAccelerated,attr"`
+    Icon string `xml:"icon,attr"`
+    KillAfterRestore bool `xml:"killAfterRestore,attr"`
+    LargeHeap bool `xml:"largeHeap,attr"`
+    Label string `xml:"label,attr"`
+    Logo int `xml:"logo,attr"`
+    ManageSpaceActivity string `xml:"manageSpaceActivity,attr"`
+    Name string `xml:"name,attr"`
+    Permission string `xml:"permission,attr"`
+    Persistent bool `xml:"persistent,attr"`
+    Process string `xml:"process,attr"`
+    RestoreAnyVersion bool `xml:"restoreAnyVersion,attr"`
+    RequiredAccountType string `xml:"requiredAccountType,attr"`
+    RestrictedAccountType string `xml:"restrictedAccountType,attr"`
+    SupportsRtl bool `xml:"supportsRtl,attr"`
+    TaskAffinity string `xml:"taskAffinity,attr"`
+    TestOnly bool `xml:"testOnly,attr"`
+    Theme int `xml:"theme,attr"`
+    UiOptions string `xml:"uiOptions,attr"`
+    VmSafeMode bool `xml:"vmSafeMode,attr"`
+}
+
 /* 
+
     <manifest xmlns:android="http://schemas.android.com/apk/res/android"
           package="string"
           android:sharedUserId="string"
@@ -14,31 +61,13 @@ package apk
     <permission> 
     <permission-group> 
     <permission-tree> 
-    <supports-gl-texture><supports-screens> 
+    <supports-gl-texture>
+    <supports-screens> 
     <uses-configuration> 
     <uses-feature> 
     <uses-permission> 
     <uses-sdk>
 */
-
-
-/*
-<instrumentation android:functionalTest=["true" | "false"]
-     android:handleProfiling=["true" | "false"]
-     android:icon="drawable resource"
-     android:label="string resource"
-     android:name="string"
-     android:targetPackage="string" />
-*/
-type Instrumentation struct {
-    Name string `xml:"name,attr"`
-    Target string `xml:"targetPackage,attr"`
-}
-
-type Application struct {
-
-}
-
 type Manifest struct {
     Package string `xml:"package,attr"`
     VersionCode int `xml:"versionCode,attr"`
--- apk/xml.go
+++ apk/xml.go
@@ -3,10 +3,11 @@ package apk
 import (
     "fmt"
     "math"
+    "encoding/xml"
 )
 // decompressXML -- Parse the 'compressed' binary form of Android XML docs 
 // such as for AndroidManifest.xml in .apk files
-func DecompressXML(xml []byte) string {
+func Unmarshal(body []byte, manifest *Manifest) error {
     endDocTag := 0x00100101
     startTag :=  0x00100102
     endTag := 0x00100103
@@ -18,7 +19,7 @@ func DecompressXML(xml []byte) string {
     //   4th word is: Number of strings in string table
     // WARNING: Sometime I indiscriminently display or refer to word in 
     //   little endian storage format, or in integer format (ie MSB first).
-    numbStrings := LEW(xml, 4*4)
+    numbStrings := lew(body, 4*4)
 
     // StringIndexTable starts at offset 24x, an array of 32 bit LE offsets
     // of the length/string data in the StringTable.
@@ -31,11 +32,11 @@ func DecompressXML(xml []byte) string {
     // XMLTags, The XML tag tree starts after some unknown content after the
     // StringTable.  There is some unknown data after the StringTable, scan
     // forward from this point to the flag for the start of an XML start tag.
-    xmlTagOff := LEW(xml, 3*4)  // Start from the offset in the 3rd word.
+    bodyTagOff := lew(body, 3*4)  // Start from the offset in the 3rd word.
     // Scan forward until we find the bytes: 0x02011000(x00100102 in normal int)
-    for ii := xmlTagOff; ii < len(xml)-4; ii += 4 {
-       if (LEW(xml, ii) == startTag) { 
-           xmlTagOff = ii
+    for ii := bodyTagOff; ii < len(body)-4; ii += 4 {
+       if (lew(body, ii) == startTag) { 
+           bodyTagOff = ii
            break
        }
     } // end of hack, scanning for start of first start tag
@@ -65,43 +66,43 @@ func DecompressXML(xml []byte) string {
 
     // Step through the XML tree element tags and attributes
     var output string
-    off := xmlTagOff
+    off := bodyTagOff
     indent := 0
-    for off < len(xml) {
-        tag0 := LEW(xml, off)
-        nameSi := LEW(xml, off+5*4)
+    for off < len(body) {
+        tag0 := lew(body, off)
+        nameSi := lew(body, off+5*4)
 
         if (tag0 == startTag) { // XML START TAG
-            numbAttrs := LEW(xml, off+7*4)  // Number of Attributes to follow
+            numbAttrs := lew(body, off+7*4)  // Number of Attributes to follow
             off += 9*4;  // Skip over 6+3 words of startTag data
-            name := compXmlString(xml, sitOff, stOff, nameSi)
+            name := compXmlString(body, sitOff, stOff, nameSi)
 
             var att string
             // Look for the Attributes
             for ii := 0; ii < numbAttrs; ii++ {
-                attrNameSi := LEW(xml, off+1*4)  // AttrName String Index
-                attrValueSi := LEW(xml, off+2*4) // AttrValue Str Ind, or FFFFFFFF
-                attrResId := LEW(xml, off+4*4)  // AttrValue ResourceId or dup AttrValue StrInd
+                attrNameSi := lew(body, off+1*4)  // AttrName String Index
+                attrValueSi := lew(body, off+2*4) // AttrValue Str Ind, or FFFFFFFF
+                attrResId := lew(body, off+4*4)  // AttrValue ResourceId or dup AttrValue StrInd
                 off += 5*4;  // Skip over the 5 words of an attribute
 
-                attrName := compXmlString(xml, sitOff, stOff, attrNameSi)
+                attrName := compXmlString(body, sitOff, stOff, attrNameSi)
 
                 var attrValue string
                 if attrValueSi != 0xffffffff {
-                    attrValue = compXmlString(xml, sitOff, stOff, attrValueSi)
+                    attrValue = compXmlString(body, sitOff, stOff, attrValueSi)
                 } else {
                     attrValue = fmt.Sprintf("%d", attrResId)
                 }
                 att = fmt.Sprintf("%s %s=\"%s\"", att, attrName, attrValue)
             }
 
-            output = fmt.Sprintf("%s%s<%s%s>\n", output, Indent(indent), name, att)
+            output = fmt.Sprintf("%s%s<%s%s>\n", output, computeIndent(indent), name, att)
             indent++;
         } else if (tag0 == endTag) { // XML END TAG
             indent--
             off += 6*4  // Skip over 6 words of endTag data
-            name := compXmlString(xml, sitOff, stOff, nameSi)
-            output = fmt.Sprintf("%s%s</%s>\n", output, Indent(indent), name)
+            name := compXmlString(body, sitOff, stOff, nameSi)
+            output = fmt.Sprintf("%s%s</%s>\n", output, computeIndent(indent), name)
 
         } else if (tag0 == endDocTag) {  // END OF XML DOC TAG
             break;
@@ -111,7 +112,7 @@ func DecompressXML(xml []byte) string {
         }
     } // end of while loop scanning tags and attributes of XML tree
     //fmt.Printf("%s    end at offset %d\n", output, off)
-    return output
+    return xml.Unmarshal([]byte(output), manifest)
 } // end of decompressXML
 
 
@@ -119,11 +120,11 @@ func compXmlString(xml []byte, sitOff int, stOff int, strInd int) string{
   if strInd < 0  {
       return "";
   }
-  strOff := stOff + LEW(xml, sitOff+strInd*4)
+  strOff := stOff + lew(xml, sitOff+strInd*4)
   return compXmlStringAt(xml, strOff)
 }
 
-func Indent(indent int) string {
+func computeIndent(indent int) string {
     spaces := string("                                             ")
     m := int(math.Min(float64(indent*2), float64(len(spaces))))
     return spaces[:m]
@@ -144,8 +145,8 @@ func compXmlStringAt(arr []byte, strOff int) string {
 } // end of compXmlStringAt
 
 
-// LEW -- Return value of a Little Endian 32 bit word from the byte array
+// lew -- Return value of a Little Endian 32 bit word from the byte array
 //   at offset off.
-func LEW(arr []byte, off int) int {
+func lew(arr []byte, off int) int {
   return int(arr[off+3])<<24&0xff000000 | int(arr[off+2])<<16&0xff0000 | int(arr[off+1])<<8&0xff00 | int(arr[off]) & 0xFF
-} // end of LEW
+} // end of lew
